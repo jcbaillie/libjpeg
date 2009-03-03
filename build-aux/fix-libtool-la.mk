08debb410af713bd781098b0c768c4697f81db0e
Akim Demaille <demaille@gostai.com>
2009-03-03 18:14:43 +0100

    Fix libtool libraries.
    	* build-aux/fix-libtool-la, build-aux/fix-libtool-la.mk: New,
    	copied from libport.
    	* Makefile.am: Use them.

---
+++ build-aux/fix-libtool-la.mk
@@ -0,0 +1,11 @@
+# See fix-libtool-la for more documentation.  Basically, just include
+# this file where Libtool libraries are defined.
+
+%.la.stamp: %.la
+	$(build_aux_dir)/fix-libtool-la $< $@
+
+LTLIBRARIES_STAMPS = $(LTLIBRARIES:=.stamp)
+CLEANFILES += $(LTLIBRARIES_STAMPS) $(LTLIBRARIES:=.bak)
+EXTRA_DIST += $(build_aux_dir)/fix-libtool-la
+all-local: $(LTLIBRARIES_STAMPS)
+
